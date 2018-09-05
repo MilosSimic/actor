da19db59bb3e2a02cb3959b7f340ab4fda96842c
milossimic <milossimicsimo@gmail.com>
2018-09-05 20:47:10 +0200

    changes

--- common.go
+++ common.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"context"
+	// "context"
 	"strings"
 )
 
-func (s *System) Terminate() {
-	s.cancel()
-}
+// func (s *System) Terminate() {
+// 	s.cancel()
+// }
 
 func join(name string) string {
 	system := "system"
@@ -15,47 +15,47 @@ func join(name string) string {
 	return strings.Join([]string{system, name}, "/")
 }
 
-func (a *ActorProp) clean(remove bool) {
-	close(a.kill)
-	close(a.box)
+// func (a *ActorProp) clean(remove bool) {
+// 	close(a.kill)
+// 	close(a.box)
 
-	if remove {
-		delete(a.Context.Actors, a.name)
-	}
-}
+// 	if remove {
+// 		delete(a.Context.Actors, a.name)
+// 	}
+// }
 
-func (a *ActorProp) Kill() {
-	a.kill <- true
-}
+// func (a *ActorProp) Kill() {
+// 	a.kill <- true
+// }
 
-func (a *ActorProp) Tell(msg interface{}) {
-	a.box <- msg
-}
+// func (a *ActorProp) Tell(msg interface{}) {
+// 	a.box <- msg
+// }
 
-func (a *ActorProp) Replay(msg interface{}) {
-	a.resp <- msg
-}
+// func (a *ActorProp) Replay(msg interface{}) {
+// 	a.resp <- msg
+// }
 
-func (a *ActorProp) Resp() interface{} {
-	return <-a.resp
-}
+// func (a *ActorProp) Resp() interface{} {
+// 	return <-a.resp
+// }
 
-func newProp(name string, system *System) *ActorProp {
-	return &ActorProp{
-		name:    name,
-		box:     make(chan interface{}),
-		resp:    make(chan interface{}),
-		kill:    make(chan bool),
-		Context: system,
-	}
-}
+// func newProp(name string, system *System) *ActorProp {
+// 	return &ActorProp{
+// 		name:    name,
+// 		box:     make(chan interface{}),
+// 		resp:    make(chan interface{}),
+// 		kill:    make(chan bool),
+// 		Context: system,
+// 	}
+// }
 
-func NewSystem(name string) *System {
-	ctx, cancel := context.WithCancel(context.Background())
-	return &System{
-		Name:   name,
-		Actors: map[string]*ActorProp{},
-		ctx:    ctx,
-		cancel: cancel,
-	}
-}
+// func NewSystem(name string) *System {
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	return &System{
+// 		Name:   name,
+// 		Actors: map[string]*ActorProp{},
+// 		ctx:    ctx,
+// 		cancel: cancel,
+// 	}
+// }
--- main.go
+++ main.go
@@ -1,55 +1,49 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"github.com/milossimic/actor/system"
 	"time"
 )
 
-type Context struct {
-	context *System
-	sender  *ActorProp
-	msg     interface{}
-}
+// type Actor interface {
+// 	Receive(context *ActorProp, msg interface{})
+// }
 
-type Actor interface {
-	Receive(context *ActorProp, msg interface{})
-}
+// type ActorProp struct {
+// 	Context *System
+// 	name    string
+// 	box     chan interface{}
+// 	resp    chan interface{}
+// 	kill    chan bool
+// }
 
-type ActorProp struct {
-	Context *System
-	name    string
-	box     chan interface{}
-	resp    chan interface{}
-	kill    chan bool
-}
-
-func (a *ActorProp) start(ctx context.Context, actor Actor) {
-	for {
-		select {
-		case <-a.kill:
-			a.clean(true)
-			return
-		case msg := <-a.box:
-			actor.Receive(a, msg)
-		case <-ctx.Done():
-			a.clean(false)
-			return
-		}
-	}
-}
+// func (a *ActorProp) start(ctx context.Context, actor Actor) {
+// 	for {
+// 		select {
+// 		case <-a.kill:
+// 			a.clean(true)
+// 			return
+// 		case msg := <-a.box:
+// 			actor.Receive(a, msg)
+// 		case <-ctx.Done():
+// 			a.clean(false)
+// 			return
+// 		}
+// 	}
+// }
 
 type Message interface {
 	Name() string
 	Params() map[string][]byte
 }
 
-type System struct {
-	Name   string
-	Actors map[string]*ActorProp
-	ctx    context.Context
-	cancel context.CancelFunc
-}
+// type System struct {
+// 	Name   string
+// 	Actors map[string]*ActorProp
+// 	ctx    context.Context
+// 	cancel context.CancelFunc
+// }
 
 func (s *System) ActorOf(name string, val Actor) *ActorProp {
 	_, ok := interface{}(val).(Actor) // test does val implement Actor interfce
@@ -89,8 +83,8 @@ func (m MyActor) Receive(context *ActorProp, msg interface{}) {
 }
 
 func main() {
-	system := NewSystem("TestSystem")
-	prop := system.ActorOf("MyActor", MyActor{})
+	sys := system.New("TestSystem")
+	prop := sys.ActorOf("MyActor", MyActor{})
 
 	prop.Tell(MyMessage{})
 	fmt.Println("Resp:", prop.Resp())
