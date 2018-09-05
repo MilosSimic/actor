6b53420365e47c7a8585dabf0cd1d1ea7a4eaaaa
milossimic <milossimicsimo@gmail.com>
2018-09-05 20:50:58 +0200

    revert changes

--- main.go
+++ main.go
@@ -2,48 +2,46 @@ package main
 
 import (
 	"fmt"
-	"github.com/milossimic/actor/system"
 	"time"
 )
 
-// type Actor interface {
-// 	Receive(context *ActorProp, msg interface{})
-// }
+type Actor interface {
+	Receive(msg interface{})
+}
 
-// type ActorProp struct {
-// 	Context *System
-// 	name    string
-// 	box     chan interface{}
-// 	resp    chan interface{}
-// 	kill    chan bool
-// }
+type ActorProp struct {
+	Context *System
+	name    string
+	box     chan interface{}
+	kill    chan bool
+}
 
-// func (a *ActorProp) start(ctx context.Context, actor Actor) {
-// 	for {
-// 		select {
-// 		case <-a.kill:
-// 			a.clean(true)
-// 			return
-// 		case msg := <-a.box:
-// 			actor.Receive(a, msg)
-// 		case <-ctx.Done():
-// 			a.clean(false)
-// 			return
-// 		}
-// 	}
-// }
+func (a *ActorProp) start(ctx context.Context, actor Actor) {
+	for {
+		select {
+		case <-a.kill:
+			a.clean(true)
+			return
+		case msg := <-a.box:
+			actor.Receive(msg)
+		case <-ctx.Done():
+			a.clean(false)
+			return
+		}
+	}
+}
 
 type Message interface {
 	Name() string
 	Params() map[string][]byte
 }
 
-// type System struct {
-// 	Name   string
-// 	Actors map[string]*ActorProp
-// 	ctx    context.Context
-// 	cancel context.CancelFunc
-// }
+type System struct {
+	Name   string
+	Actors map[string]*ActorProp
+	ctx    context.Context
+	cancel context.CancelFunc
+}
 
 func (s *System) ActorOf(name string, val Actor) *ActorProp {
 	_, ok := interface{}(val).(Actor) // test does val implement Actor interfce
@@ -72,22 +70,20 @@ func (m MyMessage) Params() map[string][]byte {
 	return nil
 }
 
-func (m MyActor) Receive(context *ActorProp, msg interface{}) {
+func (m MyActor) Receive(msg interface{}) {
 	switch conv := msg.(type) {
 	case MyMessage:
 		fmt.Println("Hello ", conv.Name())
-		context.Replay("Replay")
 	default:
 		fmt.Println("bla")
 	}
 }
 
 func main() {
-	sys := system.New("TestSystem")
-	prop := sys.ActorOf("MyActor", MyActor{})
+	system := NewSystem("TestSystem")
+	prop := system.ActorOf("MyActor", MyActor{})
 
-	prop.Tell(MyMessage{})
-	fmt.Println("Resp:", prop.Resp())
+	prop.Tell(nil)
 
 	time.Sleep(time.Second)
 
